85de85f8e7b9b885c0449a9efd1fe76f140dddbc
sureshdec <39115286+sureshdec@users.noreply.github.com>
2018-05-11 15:22:36 +0530

    ok
    ok

--- Scripts/makeZipFiles.sh
+++
@@ -1,7 +0,0 @@
-echo "Make Zip Files"
-cd bin
-7z a eCommercehybris.7z custom
-mv eCommercehybris.7z  /opt/files/sib/repo/
-cd ../envconfigs
-7z a hybrisdev.7z hybrisdev
-mv hybrisdev.7z /opt/files/sib/repo/
